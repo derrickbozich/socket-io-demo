f94b2e11fbac464ac0d1c9c754cae595fcb98f2f
Derrick Bozich <dbozich1@gmail.com>
2021-09-29 13:21:45 -0600

    socket provider setup

---
+++ src/pages/Room/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useParams, useHistory } from 'react-router-dom';
+
+
+const RoomsPage = ({ userId }) => {
+    const { room, roomId } = useParams();
+
+
+    return (
+        <main>
+            YOOO
+            {room}
+            {roomId}
+
+        </main>
+    );
+};
+
+RoomsPage.propTypes = {
+    userId: PropTypes.string.isRequired,
+};
+export default RoomsPage;
